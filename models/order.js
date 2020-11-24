3e00bd32f4e3e45445d160355d239a4bab26a21d
Cesar Martinez <chernanma@msn.com>
2020-11-23 20:40:22 -0500

    Creating model for order table

---
+++ models/order.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+  const Order = sequelize.define("Order", {
+    total: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    discount: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    tax: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    shipping: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+
+  Order.associate = function(models) {
+    // Associating Customer with Order
+    // When an Customer is deleted, also delete any associated Order
+    Order.hasMany(models.Payment, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Order;
+};
