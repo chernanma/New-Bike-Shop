48f908d0ccb14744188ff8909bac0698742c641c
Cesar Martinez <cesaguma@hotmail.com>
2020-11-23 21:06:39 -0500

    Creating brand.js model

---
+++ models/brand.js
@@ -0,0 +1,20 @@
+module.exports = function(sequelize, DataTypes) {
+    var Brand = sequelize.define("Brand", {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      }
+    });
+
+    Brand.associate = function(models) {
+        // Associating Product_type with Product
+        Brand.hasMany(models.Product, {
+          onDelete: "cascade"
+        });
+      };
+    return Brand;
+  };
+  
