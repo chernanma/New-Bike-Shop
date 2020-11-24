eca6301ebe37cd383da91483c797387f73dec287
Cesar Martinez <cesaguma@hotmail.com>
2020-11-23 21:17:59 -0500

    Formating code

--- models/brand.js
+++ models/brand.js
@@ -1,20 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
-    var Brand = sequelize.define("Brand", {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
+  const Brand = sequelize.define("Brand", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
-    });
+    }
+  });
 
-    Brand.associate = function(models) {
-        // Associating Product_type with Product
-        Brand.hasMany(models.Product, {
-          onDelete: "cascade"
-        });
-      };
-    return Brand;
+  Brand.associate = function(models) {
+    // Associating Product_type with Product
+    Brand.hasMany(models.Product, {
+      onDelete: "cascade"
+    });
   };
-  
+  return Brand;
+};
